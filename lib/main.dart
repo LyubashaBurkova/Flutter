201235c3472232b20e42d247d820eaf91953d5af
Lyubov Burkova <lyubashaburkova@gmail.com>
2020-12-15 15:58:39 +0500

    style

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Text("Hello world"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter tutorial"),
+          centerTitle: true,
+          backgroundColor: Colors.green[400],
+        ),
+        body: Text(
+          "Hello world",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.underline,
+            letterSpacing: 3.0,
+            color: Colors.black87,
+            fontFamily: "Lobster",
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("Add"),
+          backgroundColor: Colors.green[800],
+        ),
+      ),
     ),
   );
 }
