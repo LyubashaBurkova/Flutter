586c5a9a978a84dc86c124278d506e30e8b3b14b
Lyubov Burkova <lyubashaburkova@gmail.com>
2020-12-16 16:01:45 +0500

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/custom_icons.dart';
 
@@ -13,18 +14,48 @@ class FlutterTutorialApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.green[400],
         ),
-        body: Center(
-          child: IconButton(
-            icon: Icon(
-              CustomIcons.beach_access,
-              size: 50.0,
-              color: Colors.red,
+        body: Container(
+          child: Container(
+            //color: Colors.yellow[300],
+            margin: EdgeInsets.all(40),
+            padding: EdgeInsets.all(40),
+            transform: Matrix4.rotationZ(0.05),
+            decoration: BoxDecoration(
+              color: Colors.yellow[100],
+              //borderRadius: BorderRadius.all(Radius.circular((50))),
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 5,
+                color: Colors.blue[200],
+                style: BorderStyle.solid,
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.blue[200],
+                    spreadRadius: 20,
+                    blurRadius: 20,
+                    offset: Offset(10, 3))
+              ],
             ),
-            color: Colors.grey[900],
-            iconSize: 100.0,
-            onPressed: () {
-              print("Button has been pressed");
-            },
+            child: IconButton(
+              icon: Icon(
+                CustomIcons.beach_access,
+                size: 50.0,
+                color: Colors.red,
+              ),
+              iconSize: 100.0,
+              onPressed: () {
+                print("Button has been pressed");
+              },
+            ),
+          ),
+          alignment: Alignment.topLeft,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/fox.jpg"),
+              //fit: BoxFit.fill,//на весь экран
+            ),
+            //gradient:  RadialGradient(colors: [Colors.yellow[100], Colors.blue])),
           ),
         ),
         floatingActionButton: FloatingActionButton(
