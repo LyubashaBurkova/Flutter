868db32eeb72105ee4d1e641ea4bebf58602347f
Lyubov Burkova <lyubashaburkova@gmail.com>
2020-12-16 17:04:03 +0500

    SizedBox, Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,34 @@ class FlutterTutorialApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.green[400],
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.baseline,//линия выравнивания текста
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //линия выравнивания текста
             //для нее обязателен textBaseline
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Container(
-                color: Colors.blueAccent,
-                padding: EdgeInsets.all(30),
-                child: Text("1")
-            ),
-            Container(
-                color: Colors.orangeAccent,
-                padding: EdgeInsets.all(50),
-                child: Text("1")
-            ),
-            Container(
-                color: Colors.blueAccent,
-                padding: EdgeInsets.all(80),
-                child: Text("1")
-            ),
-          ]
-        ),
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Expanded(
+                flex:1,
+                child: Container(
+                    color: Colors.blueAccent,
+                    padding: EdgeInsets.all(30),
+                    child: Text("1")),
+              ),
+              SizedBox(height: 100),
+              Container(
+                  color: Colors.orangeAccent,
+                  padding: EdgeInsets.all(50),
+                  child: Text("1")),
+              SizedBox(height: 20),
+              Expanded(
+                flex: 2,
+                child: Container(
+                    color: Colors.blueAccent,
+                    padding: EdgeInsets.all(60),
+                    child: Text("1")),
+              ),
+            ]),
         floatingActionButton: FloatingActionButton(
             child: Text("Add"),
             backgroundColor: Colors.green[800],
