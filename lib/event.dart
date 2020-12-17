a0c2d3f1d357c841c9ad9967c0201e964ce6a7ad
Lyubov Burkova <lyubashaburkova@gmail.com>
2020-12-17 15:21:47 +0500

    Card

---
+++ lib/event.dart
@@ -0,0 +1,9 @@
+class Event {
+  String name;
+  String location;
+  DateTime startDateTime;
+
+  Event({this.name, this.location, this.startDateTime});
+}
+
+//var e = Event(location: "d;lf", name: "el", startDateTime: DateTime.now());
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/custom_icons.dart';
+import 'package:flutter_app/event.dart';
 
 void main() => runApp(FlutterTutorialApp());
 
 class FlutterTutorialApp extends StatelessWidget {
-  final List<String> events = [
-    "Events 1",
-    "Events 2",
-    "Events 3",
-    "Events 4",
-    "Events 5",
-    "Events 6",
-    "Events 7",
-    "Events 8",
-    "Events 9",
+  final List<Event> events = [
+    Event(
+        name: "Event 1", location: "location 1", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 2", location: "location 2", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 3", location: "location 3", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 4", location: "location 4", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 5", location: "location 5", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 6", location: "location 6", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 7", location: "location 7", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 8", location: "location 8", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 9", location: "location 9", startDateTime: DateTime.now()),
+    Event(
+        name: "Event 10",
+        location: "location 10",
+        startDateTime: DateTime.now()),
+    Event(
+        name: "Event 11",
+        location: "location 11",
+        startDateTime: DateTime.now()),
+    Event(
+        name: "Event 12",
+        location: "location 12",
+        startDateTime: DateTime.now()),
   ];
 
   @override
@@ -26,22 +48,46 @@ class FlutterTutorialApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.green[400],
         ),
-        body: ListView.separated(
+        body: ListView.builder(
           //reverse: true,
           //controller: ScrollController(initialScrollOffset: 150),
           physics: BouncingScrollPhysics(),
           //physics: NeverScrollableScrollPhysics(),//отключаем скролл
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(10),
           itemCount: events.length,
-          itemBuilder: (_, index) => Text(
-            events[index],
-            style: TextStyle(fontSize: 100),
+          itemBuilder: (_, index) => Card(
+            color: Colors.yellow[300],
+            elevation: 10,//на сколько карточка поднята (тень)
+            shadowColor: Colors.blue,
+            margin: EdgeInsets.symmetric(vertical: 20.0, horizontal:10.0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                Text(
+                  //events[index].location,//events[index] - объект
+                  "${events[index].name}",
+                  style: TextStyle(fontSize: 30),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "location - ${events[index].location}, ${events[index].startDateTime}",
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(events[index].location),
+                    SizedBox(width: 10),
+                    Text(events[index].startDateTime.toString()),
+                  ],
+                ),
+              ],
+            ),
           ),
-          separatorBuilder: (_, ___) => Divider(color: Colors.red, thickness: 5,),
-          // separatorBuilder: (_, ___) => Text(
-          //   "_____________________________",
-          //   style: TextStyle(fontSize: 30),
-          // ),
         ),
         floatingActionButton: FloatingActionButton(
             child: Text("Add"),
